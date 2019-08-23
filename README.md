17f1648aaed7b0aaba1fb2252ed3e1f5638d70f4
Mickael Ly <mickael.ly@inria.fr>
2019-08-23 15:26:26 +0200

    Update doc

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
+
 # PyGLViewer
 
-Framework developed for the ENS Course [...](...)
+Framework developed for the course [Numerical mechanics: From Lagrangian mechanics to simulation tools for computer graphics](https://team.inria.fr/elan/classes/) given with Florence Bertails-Descoubes & Mélina Skouras.
 
 This is a simple OpenGL viewer designed to have a quick visualization of the results of the simulations. The goal is *not* to do efficient/marvelous renderings, although you can tune a bit the viewer if you want.
 
@@ -19,12 +20,20 @@ This is a simple OpenGL viewer designed to have a quick visualization of the res
 
 To install them (on Ubuntu) :
 ```
-sudo apt-get install python3 python3-setuptools libglfw3-dev libassimp-dev python3-pip
-pip3 install numpy Pillow pyassimp==4.1.3 PyOpenGL PyOpenGL-accelerate glfw
+sudo apt-get install freeglut3 freeglut3-dev mesa-utils python3 python3-setuptools libglfw3-dev libassimp-dev python3-pip
+pip3 install numpy Pillow pyassimp==4.1.3 PyOpenGL PyOpenGL-accelerate glfw scipy
 ```
 **Warning:** Be careful with the version of PyAssimp, the latest version (4.1.4) crashes...
 
 
-### Quick tutorial
+### Test
 
-...
+Running
+```
+python3 main.py
+```
+should play one of the default base scenarios.
+
+### Making your own scenarios
+
+The examples of the file `scene.py` are quite self explanatory. 
--- code/scenes.py
+++ code/scenes.py
@@ -10,52 +10,72 @@ from geom import *
 
 
 def indexedTest(viewer):
+    """
+    @brief Demonstration for a basic static rendering
+           Renders a simple square 
+    """
 
     # Indexed square
-    positions = np.array([0., 0.,
-                          1., 0.,
-                          0., 1.,
-                          1., 1.],
+    positions = np.array([0., 0.,   # x0, y0
+                          1., 0.,   # x1, y1
+                          0., 1.,   # x2, y2
+                          1., 1.],  # x3, y3
                          np.float64)
-    colours = np.array([1., 0., 0.,
-                        0., 0., 1.,
-                        0., 1., 0.,
-                        1., 1., 1.])
-    indices = np.array([0, 1, 2,   # First triangle
-                        1, 2, 3])  # Second triangle
+    colours = np.array([1., 0., 0.,  # (r, g, b) for vertex 0
+                        0., 0., 1.,  # (r, g, b) for vertex 1
+                        0., 1., 0.,  # ...
+                        1., 1., 1.]) # ...
+    indices = np.array([0, 1, 2,   # First triangle composed by vertices 0, 1 and 2
+                        1, 2, 3])  # Second triangle composed by vertices 1, 2 and 3
 
+    # Create the object
     squareMesh = Mesh2D(positions, indices, colours)
+    # Create the correspondung GPU object
     squareMeshRenderable = Mesh2DRenderable(squareMesh)
-    
+    # Add it to the list of objects to render
     viewer.addRenderable(squareMeshRenderable)
 
 def dynamicTest(viewer):
-
+    """
+    @brief Demonstration for a basic dynamic rendering
+           Renders a simple square, moved by a dummy dynamic system
+    """
 
     # Indexed square
-    positions = np.array([0., 0.,
-                          1., 0.,
-                          0., 1.,
-                          1., 1.],
+    positions = np.array([0., 0.,   # x0, y0
+                          1., 0.,   # x1, y1
+                          0., 1.,   # x2, y2
+                          1., 1.],  # x3, y3
                          np.float64)
-    colours = np.array([1., 0., 0.,
-                        0., 0., 1.,
-                        0., 1., 0.,
-                        1., 1., 1.])
-    indices = np.array([0, 1, 2,   # First triangle
-                        1, 2, 3])  # Second triangle
+    colours = np.array([1., 0., 0.,  # (r, g, b) for vertex 0
+                        0., 0., 1.,  # (r, g, b) for vertex 1
+                        0., 1., 0.,  # ...
+                        1., 1., 1.]) # ...
+    indices = np.array([0, 1, 2,   # First triangle composed by vertices 0, 1 and 2
+                        1, 2, 3])  # Second triangle composed by vertices 1, 2 and 3
 
+    # Create the object
     squareMesh = Mesh2D(positions, indices, colours)
-    dyn = DummyDynamicSystem(squareMesh)
-    viewer.addDynamicSystem(dyn)
-    
+    # Create the correspondung GPU object
     squareMeshRenderable = Mesh2DRenderable(squareMesh)
+    # Add it to the list of objects to render
     viewer.addRenderable(squareMeshRenderable)
 
+    # Create a dynamic system
+    dyn = DummyDynamicSystem(squareMesh)
+    # And add it to the viewer
+    # Each frame will perform a call to the 'step' method of the viewer
+    viewer.addDynamicSystem(dyn)
+    
 
 
 def rodTest(viewer):
 
+    """
+    @brief Demonstration for a rendering of a rod object
+           Specific case, as a rod is essentialy a line, we
+           need to generate a mesh over it to git it a thickness
+    """
     positions = np.array([-1., 1.,
                           -1., 0.,
                           -0.5, -0.25],
@@ -65,14 +85,18 @@ def rodTest(viewer):
                         0., 0., 1.])
 
     rod = Rod2D(positions, colours)
-    #dyn = DummyDynamicSystem(rod)
-    #viewer.addDynamicSystem(dyn)
 
-    rodRenderable = Rod2DRenderable(rod, 0.005)
+    rodRenderable = Rod2DRenderable(rod, thickness = 0.005)
     viewer.addRenderable(rodRenderable)
     
 
 def loadTest(viewer):
+
+    """
+    @brief Demonstration for the loading of any 2D mesh
+           Be careful, the function loadMeshes has been mofified
+           to deal with 2D meshes only (z will be considered to be 0)
+    """
 
     meshes2D = loadMeshes("./mesh/triangle.obj")
 
